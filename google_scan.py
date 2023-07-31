4385eb2b244c99f7ea37eb882dc5544a0c21288e
tcolinbright <117130462+tcolinbright@users.noreply.github.com>
2023-07-30 17:01:05 -0700

    scan results

--- google_scan.py
+++ google_scan.py
@@ -1,16 +1,15 @@
-
-
 import csv
 import requests
-from creds import *
-
-
+from creds import api_key
 
 def get_book_info(isbn):
     response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={api_key}')
     return response.json()
 
 def parse_book_info(json_response):
+    if json_response['totalItems'] == 0:
+        return None
+
     volume_info = json_response['items'][0]['volumeInfo']
     title = volume_info.get('title')
     authors = volume_info.get('authors')
@@ -24,6 +23,11 @@ def write_book_info_to_csv(isbn, bin_number, book_info):
         writer = csv.writer(f)
         writer.writerow([isbn, bin_number] + list(book_info))
 
+def write_isbn_to_csv(isbn):
+    with open('unfound_books.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([isbn])
+
 def main():
     while True:
         isbn = input('Enter ISBN (or "q" to quit): ')
@@ -32,7 +36,17 @@ def main():
         bin_number = input('Enter bin number: ')
         book_info = get_book_info(isbn)
         parsed_book_info = parse_book_info(book_info)
-        write_book_info_to_csv(isbn, bin_number, parsed_book_info)
+
+        if parsed_book_info is None:
+            print('No information found for this ISBN.')
+            write_isbn_to_csv(isbn)
+        else:
+            print(f'Found book information: {parsed_book_info}')
+            confirmation = input('Is this information correct? (Y/n): ')
+            if confirmation.lower() == 'n':
+                print('Please re-enter the correct ISBN.')
+            else:
+                write_book_info_to_csv(isbn, bin_number, parsed_book_info)
 
 if __name__ == "__main__":
     main()
